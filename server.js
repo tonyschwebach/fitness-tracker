f60afe70c1bd98064c3522272ab3ba5b98e5d3b4
T Schway <tony.schwebach@gmail.com>
2021-02-05 15:02:40 -0500

    setup route for index homepage

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
+// require npm packages
+
 const express = require("express");
 const mongoose = require("mongoose");
-// const Place = require("./models/Place");
 
+// require all models
+// const Place = require("./models/Place");
+// const db = require("./models");
+
+// create an instance of express
 const app = express();
 
+// create a port
 const PORT = process.env.PORT || 8080;
 
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
+// connect to mongodb
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness_tracker", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -23,10 +38,14 @@ connection.on("error", (err) => {
   console.log("Mongoose connection error: " + err);
 });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
+// use routes on controllers
+// app.use(UserController);
 
 // TODO: ABSTRACT THESE PLACES ROUTES OUT INTO A CONTROLLER
+app.get("/",(req,res)=>{
+  res.send("/index.html")
+})
 
 // app.get("/api/places", (req, res) => {
 //   Place.find().then((allPlaces) => {
